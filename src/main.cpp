ad9f723fc3c69a6bca2ddf1929f8fc03d8dbe2dc
Maíra Matos <maira.matos@outlook.com.br>
2020-11-01 18:52:25 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,164 @@
-#include <mbed.h>
+#include "mbed.h"
 
-int main() {
+InterruptIn button(p5);
 
-  // put your setup code here, to run once:
+DigitalOut red_(p6);
+DigitalOut yellow_(p7);
+DigitalOut green_(p8); // Representado pelo led azul
 
-  while(1) {
-    // put your main code here, to run repeatedly:
-  }
+Ticker ticker;
+Timeout timeout;
+
+enum States
+{
+    OFF,
+    RED,
+    YELLOW,
+    GREEN,
+    ALLERT,
+};
+
+States state = RED;
+States state_ = RED;
+
+float button_timer = 0;
+bool press = false;
+
+//Trafic Light
+void red();
+void yellow();
+void green();
+
+//Button
+void start_button();
+void end_button();
+
+void off();
+void allert();
+void revert_state();
+void reset();
+
+
+void start_button()
+{
+    button_timer = 0;
+    press = true;
+}
+
+void end_button()
+{
+    printf("Botão pressionado por %.3fs\n", button_timer);
+    printf("--------------------------------------------------\n");
+    
+    if (button_timer <= 3 && state == GREEN)
+        yellow();
+    else if (button_timer <= 10)
+    {
+	if (state == ALLERT) revert_state();
+	else allert();
+        
+    }
+    else
+    {
+	if (state == ALLERT) red();
+	else off();
+    }
+    press = false;
+}
+
+void off()
+{
+    reset();
+    printf("State : Off\n");
+    printf("__________________________________________________\n");
+    state = OFF;
+}
+
+void red()
+{
+    reset();
+    printf("State : Red\n");
+    printf("__________________________________________________\n");
+    red_ = 1;
+    state = RED;
+    timeout.attach(&green, 10);
+}
+
+void yellow()
+{
+    reset();
+    printf("State : Yellow\n");
+    printf("__________________________________________________\n");
+    yellow_ = 1;
+    state = YELLOW;
+    timeout.attach(&red, 4);
+}
+
+void green()
+{
+    reset();
+    printf("State : Green\n");
+    printf("__________________________________________________\n");
+    green_ = 1;
+    state = GREEN;
+    timeout.attach(&yellow, 20);
+}
+
+void blinkYellow()
+{
+    yellow_ = !yellow_;
+}
+
+void allert()
+{
+    reset();
+    printf("State : Allert\n");
+    printf("__________________________________________________\n");
+    state_ = state;
+    state = ALLERT;
+    ticker.attach(&blinkYellow, 0.5);
+}
+
+void revert_state()
+{
+    reset();
+    switch (state_)
+    {
+    case OFF:
+        off();
+        break;
+    case RED:
+        red();
+        break;
+    case YELLOW:
+        yellow();
+        break;
+    case GREEN:
+        green();
+        break;
+    default:
+        break;
+    }
+}
+
+void reset()
+{
+    red_ = 0;
+    yellow_ = 0;
+    green_ = 0;
+    ticker.detach();
+    timeout.detach();
+}
+
+int main()
+{
+    red();
+    button.rise(&start_button);
+    button.fall(&end_button);
+    while (1)
+    {
+        if (press)
+            button_timer += 0.1;
+        wait(0.1);
+    }
 }
